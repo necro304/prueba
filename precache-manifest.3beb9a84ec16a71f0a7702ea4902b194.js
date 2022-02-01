e7f50bde643a077e0e4903bfb5dd62c2bc8dae2d
necro304 <31376673+necro304@users.noreply.github.com>
2022-02-01 17:37:36 -0500

    deploy - 02/01/2022

---
+++ precache-manifest.3beb9a84ec16a71f0a7702ea4902b194.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26aa4d76434144ecccf97e26eae8eee1",
+    "url": "/apexcharts.min.js"
+  },
+  {
+    "revision": "bbbe20f2791a94dee25c",
+    "url": "/css/app.deb7d4c2.css"
+  },
+  {
+    "revision": "1b7e7af3095d5dfa6052",
+    "url": "/css/chunk-14340e62.e0ff0825.css"
+  },
+  {
+    "revision": "039e3bc36782d602d057",
+    "url": "/css/chunk-1e30b5ee.f45112db.css"
+  },
+  {
+    "revision": "a6361bb6918754ebf821",
+    "url": "/css/chunk-1e9b707a.c4718041.css"
+  },
+  {
+    "revision": "faa83ddc88b9a61c366f",
+    "url": "/css/chunk-2c0b195e.fc26c9fb.css"
+  },
+  {
+    "revision": "914023c92a97bb7cf7ba",
+    "url": "/css/chunk-341df140.2980908b.css"
+  },
+  {
+    "revision": "e8cba1cd92e9eb294786",
+    "url": "/css/chunk-4ef03411.2980908b.css"
+  },
+  {
+    "revision": "87707bb41dd608c65bbe",
+    "url": "/css/chunk-758ecaf0.688e6885.css"
+  },
+  {
+    "revision": "9c956809bc19b309872d",
+    "url": "/css/chunk-88f77be0.45b15f5e.css"
+  },
+  {
+    "revision": "8ac997309721d4ee0dfd",
+    "url": "/css/chunk-c737d180.983e8251.css"
+  },
+  {
+    "revision": "39e266bea6ececbb06d7",
+    "url": "/css/chunk-d52a6adc.2980908b.css"
+  },
+  {
+    "revision": "c8d952285ebc098dc0d6",
+    "url": "/css/chunk-vendors.8ae5c045.css"
+  },
+  {
+    "revision": "f3e6d7025c0c23d9fecf82420fa4d2e6",
+    "url": "/img/ab-1.f3e6d702.jpeg"
+  },
+  {
+    "revision": "7e9de3e775d4ccda3af227d77a29b7ef",
+    "url": "/img/logo.7e9de3e7.png"
+  },
+  {
+    "revision": "4c700cf3b4f2ea3029d65749bff4e7f0",
+    "url": "/img/wallpaper-login.4c700cf3.png"
+  },
+  {
+    "revision": "3ea32ce661393109a8f375e3d40a4cc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbbe20f2791a94dee25c",
+    "url": "/js/app.bdf9fb5b.js"
+  },
+  {
+    "revision": "1b7e7af3095d5dfa6052",
+    "url": "/js/chunk-14340e62.1ac2d718.js"
+  },
+  {
+    "revision": "07a36e81e454ef14c8af",
+    "url": "/js/chunk-168b7027.989b074b.js"
+  },
+  {
+    "revision": "039e3bc36782d602d057",
+    "url": "/js/chunk-1e30b5ee.45d6557a.js"
+  },
+  {
+    "revision": "a6361bb6918754ebf821",
+    "url": "/js/chunk-1e9b707a.b6d8cfad.js"
+  },
+  {
+    "revision": "ef945c7ad6ce85a25ba2",
+    "url": "/js/chunk-241a679e.274df737.js"
+  },
+  {
+    "revision": "faa83ddc88b9a61c366f",
+    "url": "/js/chunk-2c0b195e.6de1e164.js"
+  },
+  {
+    "revision": "61182e1cc641e4ba7797",
+    "url": "/js/chunk-2d0be333.4f9e18fb.js"
+  },
+  {
+    "revision": "6a5a5b18ecab245c17d6",
+    "url": "/js/chunk-2d0d3ea4.1ee46f8b.js"
+  },
+  {
+    "revision": "36194317d288c21fd892",
+    "url": "/js/chunk-2d21e000.2afdc2f8.js"
+  },
+  {
+    "revision": "fa6fd3f77bbe41e72ab6",
+    "url": "/js/chunk-2d229dc1.2b94ec35.js"
+  },
+  {
+    "revision": "914023c92a97bb7cf7ba",
+    "url": "/js/chunk-341df140.2ea80545.js"
+  },
+  {
+    "revision": "e8cba1cd92e9eb294786",
+    "url": "/js/chunk-4ef03411.22ff85a3.js"
+  },
+  {
+    "revision": "87707bb41dd608c65bbe",
+    "url": "/js/chunk-758ecaf0.23506e6a.js"
+  },
+  {
+    "revision": "b7ba5c5e31a80283aa43",
+    "url": "/js/chunk-7d2dd9e8.f896c0be.js"
+  },
+  {
+    "revision": "2b67f4c14b113609bbe9",
+    "url": "/js/chunk-8709fe62.6fae677c.js"
+  },
+  {
+    "revision": "9c956809bc19b309872d",
+    "url": "/js/chunk-88f77be0.89718738.js"
+  },
+  {
+    "revision": "d8d072ffbc27091c47b6",
+    "url": "/js/chunk-be9f3d30.5186f622.js"
+  },
+  {
+    "revision": "8ac997309721d4ee0dfd",
+    "url": "/js/chunk-c737d180.d718169f.js"
+  },
+  {
+    "revision": "5a958db6a8e9cb58c02e",
+    "url": "/js/chunk-cb3768fa.10d17774.js"
+  },
+  {
+    "revision": "39e266bea6ececbb06d7",
+    "url": "/js/chunk-d52a6adc.3f843d29.js"
+  },
+  {
+    "revision": "f7e5cfe5d7beb0a7ea1f",
+    "url": "/js/chunk-de51e312.457d8179.js"
+  },
+  {
+    "revision": "da4360daf11ec02c7cfc",
+    "url": "/js/chunk-f8fdb992.ee499f19.js"
+  },
+  {
+    "revision": "c8d952285ebc098dc0d6",
+    "url": "/js/chunk-vendors.bc988ebc.js"
+  },
+  {
+    "revision": "95758fef7753bde88b9d405d780c5294",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.3beb9a84ec16a71f0a7702ea4902b194.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "admin_todi_envios"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
